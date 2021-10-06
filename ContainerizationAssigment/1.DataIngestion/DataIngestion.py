e6c284e70628b186e874d5e02b4d52d2d95e585b
SemUijen <grootveer.sem@outlook.com>
2021-10-06 15:19:38 +0200

    wrm

--- ContainerizationAssigment/1.DataIngestion/DataIngestion.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import os 
-
-os.chdir('C:\\Users\\Elske\\OneDrive\\Documenten\\DE_Assignment1')
-
-df_test = pd.read_csv('test.csv').dropna()
-df_train = pd.read_csv('train.csv').dropna()
--- ContainerizationAssigment/2.FeatureExtractions/app.py
+++ ContainerizationAssigment/2.FeatureExtractions/app.py
@@ -6,19 +6,15 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 db_util = DBUtil()
 
-@app.route('/2.FeatureExtractions/<table_name>', methods=['GET'])
-def read_data(table_name):
-    df = db_util.read_data_records(table_name)
-    df = df.drop(columns=['id'])
-    resp = Response(df.to_json(orient='records'), status=200, mimetype='application/json')
+@app.route('/2.FeatureExtractions/DatabaseFeatures', methods=['GET'])
+def Extract_features():
+    db_api = os.environ['TRAININGDB_API']
+    # Make a GET request to training db service to retrieve the Database
+    r = requests.get(db_api)
+    j = r.json()
+    df = pd.DataFrame.from_dict(j)
+    df_new = df[['Age', 'Pclass','Survived']]
+    resp = Response(df_new.to_json(orient='records'), status=200, mimetype='application/json')
     return resp
-
-@app.route('/2.FeatureExtractions/<table_name>', methods=['POST'])
-def create_table(table_name):
-    # get the payload or body
-    req_data = request.get_json()
-    columns = req_data['columns']
-    db_util.create_tb(table_name, columns)
-    return json.dumps({'message': 'a table was created'}, sort_keys=False, indent=4), 200
 
 app.run(host='0.0.0.0', port=5000)
