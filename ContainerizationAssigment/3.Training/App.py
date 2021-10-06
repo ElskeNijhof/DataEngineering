758d9960f97fafbfcce229b10eecfe8f219c3472
ElskeNijhof <elske.nijhof@outlook.com>
2021-10-06 13:38:34 +0200

    Els10okt

--- ContainerizationAssigment/3.Training/App.py
+++ ContainerizationAssigment/3.Training/App.py
@@ -5,24 +5,22 @@ import requests
 from flask import Flask, Response
 
 from flask import jsonify
+from Resources import TrainV2
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/training-cp/model', methods=['POST'])
+@app.route('/Training/model', methods=['POST'])
 def train_models():
     db_api = os.environ['TRAININGDB_API']
     # Make a GET request to training db service to retrieve the training data/features.
     r = requests.get(db_api)
     j = r.json()
     df = pd.DataFrame.from_dict(j)
-    resp = model_trainer.train(df.values)
+    resp = TrainV2.train(df.values)
     return resp
 
 
 app.run(host='0.0.0.0', port=5000)
-© 2021 GitHub, Inc.
-Terms
-Privacy
-S
