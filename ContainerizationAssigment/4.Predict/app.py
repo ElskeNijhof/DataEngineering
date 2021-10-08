5bf7859faa0dab406bd4834f232b2c52a3fe366a
SemUijen <grootveer.sem@outlook.com>
2021-10-08 20:32:42 +0200

    ff

--- ContainerizationAssigment/4.Predict/app.py
+++ ContainerizationAssigment/4.Predict/app.py
@@ -10,11 +10,14 @@ app.config["DEBUG"] = True
 
 @app.route('/predict/Age:<age>/class:<clss>', methods=['GET'])
 def predict(age, clss):
+
+    html = "<h3>Hello, you will probably...</h3>" \
+           "<b>{liveOrDie}</b><br/>" 
     # Get Model:
     Model_api = os.environ['MODEL_API']
     Request =  requests.get(Model_api)
-    prediction = Request.predict(age, clss)
     
-    return prediction
+    result = Request.json()
+    return html.format(liveOrDie = result)
 
 app.run(host='0.0.0.0', port=500)
