c9a8d3789e41c294269cacf1ba4e29b24f06184a
ArteSpanjers <a.c.spanjers@student.tue.nl>
2021-10-08 14:40:47 +0200

    visualhtml

--- ContainerizationAssigment/5.Visualization/app.py
+++ ContainerizationAssigment/5.Visualization/app.py
@@ -9,14 +9,18 @@ app.config["DEBUG"] = True
 
 @app.route('/visualization/Age:<age>/class:<clss>', methods=['GET'])
 def Visualization(age, clss):
-    
+    #Set up returning text
+    html = "<h3>Hello, you will probably...</h3>" \
+           "<b>{liveOrDie}</b><br/>" 
+
+    #Retrieve the score that indicates 'live' or 'die'
     Prediction_api = os.environ['/predict/Age:{}/class:{}'.format(age, clss)]
     if Prediction_api >= 0.5:
-        print("gello")
-        #return alive
+        result = "live"
+        return html.format(liveOrDie = result)
     else:
-        print("gello")
-        #return you are dead
+        result = "die"
+        return html.format(liveOrDie = result)
 
 app.run(host='0.0.0.0', port=500)
    
