635186cc880f6927feb91fbe2866772c35449f47
SemUijen <grootveer.sem@outlook.com>
2021-10-08 12:34:58 +0200

    wat leuk

--- ContainerizationAssigment/1.DataIngestion/app.py
+++ ContainerizationAssigment/1.DataIngestion/app.py
@@ -11,4 +11,4 @@ def read_data():
     resp = Response(df_train.to_json(orient='records'), status=200, mimetype='application/json')
     return resp
 
-app.run(host='0.0.0.0', port=5000)
+app.run(host='0.0.0.0', port=500)
