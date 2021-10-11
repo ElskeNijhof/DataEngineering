1f29b5161b2f9dd1c5be1ec70c4cdf5bf703a934
SemUijen <grootveer.sem@outlook.com>
2021-10-11 14:58:16 +0200

    gg

--- ContainerizationAssigment/4.Predict/app.py
+++ ContainerizationAssigment/4.Predict/app.py
@@ -30,9 +30,9 @@ def predict(age, clss):
         Train_API = os.environ['Train_API']   
         TrainModel = requests.get(Train_API)
 
-        file_path = os.path.join(model_repo) 
-        os.chdir(file_path)
-        with open("Titanic_model.pkl", 'rb') as file:
+        Model_DIR = os.path.join(model_repo, "Titanic_model.pkl")
+        
+        with open(Model_DIR, 'rb') as file:
             pickle_model = pickle.load(file)    
         
         prediction = pickle_model.predict([[age,clss]])
